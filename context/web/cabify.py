32be516f8ce92d010a06d3398bbf394c5955ab95
Francisco Fregona <ffregona@santafe.gov.ar>
2018-04-19 15:24:48 -0300

    Reescrito: eliminado upstart y simplificado entrypoint a su minima expresion. Funciona OK.

--- context/web/cabify.py
+++ context/web/cabify.py
@@ -6,4 +6,6 @@ from bottle import route, run
 def status():
     return 'OK\n'
 
-run(host='localhost', port=8181, debug=False)
+# run(host='localhost', port=8181, debug=False)
+run(host='0.0.0.0', port=8181, debug=False)
+# run(host='172.17.0.2', port=8181, debug=False)
